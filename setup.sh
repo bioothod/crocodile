e71d5db0448277fa5c4e485f237ed8e972cba383
megacrab <megacrab@users.noreply.github.com>
2014-12-21 17:02:54 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 apt-get update
 apt-get install -y python-minimal python-pip
 pip install protobuf bernhard
-wget https://github.com/megacrab/tmp/blob/master/crocodile/checker.sh && bash checker.sh
+wget https://raw.githubusercontent.com/megacrab/tmp/master/crocodile/checker.sh && bash checker.sh
 rm setup.sh checker.sh
