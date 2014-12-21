d6bf58fa99b050ee30b76d62c9c8b8faffea6308
root <root@ubuntu.(none)>
2014-12-21 08:17:52 -0500

    ororo

---
+++ crocodile/conf.d/script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "{'host':'example.org', 'service':'temp-m','metric':777,'state':'normal'}"
---
+++ crocodile/conf.d/script3.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "{'host':'example.org','service':'temp-m','metric':666,'state':'error'}"
