9a6941bdcf2c001df9e60ab58c44b9b89581e9ec
megacrab <megacrab@users.noreply.github.com>
2014-12-21 17:21:24 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y python-minimal python-pip git
+apt-get install -y python-minimal python-pip git dos2unix
 pip install protobuf bernhard
-wget https://raw.githubusercontent.com/megacrab/tmp/master/crocodile/checker.sh && bash checker.sh
+wget https://raw.githubusercontent.com/megacrab/tmp/master/crocodile/checker.sh
+dos2unix checker.sh
+bash checker.sh
 rm setup.sh checker.sh
