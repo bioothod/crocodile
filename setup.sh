72c9041c137d0f12987c1e387e89a1701437d666
root <root@ubuntu.(none)>
2014-12-30 19:49:47 -0500

    ororo

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 apt-get update >/dev/null
-apt-get install -y python-minimal python-pip git dos2unix supervisor
-pip install protobuf bernhard
+apt-get install -y python-minimal python-pip git dos2unix
+pip install protobuf bernhard supervisor
 wget https://raw.githubusercontent.com/megacrab/tmp/master/crocodile/checker.sh
 dos2unix checker.sh
 bash checker.sh
