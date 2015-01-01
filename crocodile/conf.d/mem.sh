ff9222e948004ab65c0b01c34cce4de7a64ed9ea
Evgeniy Polyakov <zbr@ioremap.net>
2015-01-02 00:46:05 +0300

    mem: use bytes in 'free' call. Also, force clients to reconfigure themself, put special word: ororo

--- crocodile/conf.d/mem.sh
+++ crocodile/conf.d/mem.sh
@@ -2,8 +2,8 @@
 #check free mem
 error_perc=20
 warning_perc=30
-total=`free |grep Mem |awk {' print $2 '}`
-free=`free |grep buffers/cache |awk {' print $4 '}`
+total=`free -b |grep Mem |awk {' print $2 '}`
+free=`free -b |grep buffers/cache |awk {' print $4 '}`
 if [ $(($free * 100 / $total)) -le $error_perc ];then 
   state='error'
 elif [ $(($free * 100 / $total)) -le $warning_perc ];then 
