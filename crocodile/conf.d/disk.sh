6e6fc1981a9520550a669fad4ee99cd6edc9ec91
Evgeniy Polyakov <zbr@ioremap.net>
2015-01-01 21:59:29 +0300

    disk: emit message per disk

--- crocodile/conf.d/disk.sh
+++ crocodile/conf.d/disk.sh
@@ -3,23 +3,20 @@ error_perc=85
 warning_perc=80
 state='normal'
 
-space=`df -h`
-msg='Ok'
+space=`df -t ext4`
 while read -r line; do
 	if test x"`echo $line | grep Filesystem`" != x""; then
 		continue
 	fi
 
+	avail=`echo $line | awk {'print $4'}`
+	disk=`echo $line | awk {'print $1'}`
 	used=`echo $line | awk {'print $5'} | sed -e 's/%//g'`
 	if test $used -gt $error_perc; then
 		state='error'
-		msg="$line"
-		break
 	elif test $used -gt $warning_perc; then
 		state='warning'
-		msg="$line"
 	fi
+
+	echo "{'host':'`hostname -f`', 'service':'$disk', 'metric':'$avail', 'state':'$state', 'description': ''}"
 done <<< "$space"
-
-
-echo "{'host':'`hostname -f`', 'service':'disk', 'metric':$free, 'state':'$state', 'message': '$msg'}"
