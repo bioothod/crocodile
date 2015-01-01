35e89a86f395f8d9f318d31b47bfc9cf54e51ab5
Evgeniy Polyakov <zbr@ioremap.net>
2015-01-01 21:14:48 +0300

    disk: implemented disk usage monitoring script

--- crocodile/conf.d/disk.sh
+++ crocodile/conf.d/disk.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-echo "{'host':'example.org','service':'temp-m','metric':666,'state':'error'}"
+error_perc=85
+warning_perc=80
+state='normal'
+
+space=`df -h`
+while read -r line; do
+	if test x"`echo $line | grep Filesystem`" != x""; then
+		continue
+	fi
+
+	used=`echo $line | awk {'print $5'} | sed -e 's/%//g'`
+	if test $used -ge $error_perc; then
+		state='error'
+		break
+	elif test $used -ge $warning_perc; then
+		state='warning'
+	fi
+done <<< "$space"
+
+
+echo "{'host':'`hostname -f`', 'service':'disk', 'metric':$free, 'state':'$state', 'message': '$space'}"
