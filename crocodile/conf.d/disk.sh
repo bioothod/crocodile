f84c89459ed9d05fc6272a099bf5e896133c24e8
Evgeniy Polyakov <zbr@ioremap.net>
2015-01-01 21:32:21 +0300

    disk: put error disk info into message

--- crocodile/conf.d/disk.sh
+++ crocodile/conf.d/disk.sh
@@ -4,6 +4,7 @@ warning_perc=80
 state='normal'
 
 space=`df -h`
+msg='Ok'
 while read -r line; do
 	if test x"`echo $line | grep Filesystem`" != x""; then
 		continue
@@ -12,11 +13,13 @@ while read -r line; do
 	used=`echo $line | awk {'print $5'} | sed -e 's/%//g'`
 	if test $used -gt $error_perc; then
 		state='error'
+		msg="$line"
 		break
 	elif test $used -gt $warning_perc; then
 		state='warning'
+		msg="$line"
 	fi
 done <<< "$space"
 
 
-echo "{'host':'`hostname -f`', 'service':'disk', 'metric':$free, 'state':'$state', 'message': '$space'}"
+echo "{'host':'`hostname -f`', 'service':'disk', 'metric':$free, 'state':'$state', 'message': '$msg'}"
