d26faedc8cfcfeedade79356d0e656ee3adec062
Evgeniy Polyakov <zbr@ioremap.net>
2015-01-01 20:55:56 +0300

    mem: use free+cached+buffers memory as free, set warning limit as 30%

--- crocodile/conf.d/mem.sh
+++ crocodile/conf.d/mem.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #check free mem
-ok_perc=10
+ok_perc=30
 total=`free -m |grep Mem |awk {' print $2 '}`
-free=`free -m |grep Mem |awk {' print $4 '}`
+free=`free -m |grep buffers/cache |awk {' print $3 '}`
 if [ $(($free * 100 / $total)) -le $ok_perc ];then 
   state='warning'
 else
