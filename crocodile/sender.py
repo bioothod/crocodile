f0bf7290a7f566c74f11e91dc5675dc014c2da22
Evgeniy Polyakov <zbr@ioremap.net>
2015-01-02 18:22:46 +0300

    sender: when script name starts with number, consider it as number of seconds to sleep between runs. Default timeout is 10 seconds for every script.

--- crocodile/sender.py
+++ crocodile/sender.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 #getting list of conf.d directory
 #executing them and sending results to all riemann instances
-import bernhard, os, subprocess, socket, ast, time
+import bernhard, os, subprocess, socket, ast, time, re, logging
 
 #for except in sender function
 socket.setdefaulttimeout(2)
+
+logging.basicConfig(format='%(asctime)s %(levelname): s%(message)s', level=logging.INFO)
 
 confd = '/etc/crocodile/conf.d/'
 #list of riemann instances
@@ -25,15 +27,36 @@ def sender(message):
             pass
 
 #checking/running/sending
+scripts_timeouts = {}
 while True:
+    timeout = 10
     for x in os.listdir(confd):
-        try:
+        m = re.search('^\d+', x)
+        if m not None:
+            wake_up_time = scripts_timeouts.get(x)
+            if wake_up_time not None:
+                if time.time() < wake_up_time:
+                    tm = wake_up_time - time.time()
+                    if tm < timeout:
+                        timeout = tm
+
+                    continue
+
+            tm = int(m.group(0))
+            scripts_timeouts[x] = tm + time.time()
+
+            if tm < timeout:
+                timeout = tm
+        try
             out = run_process(confd+x)
+            logging.info('%s: completed: out: \'%s\', timeout: %d', x, out, timeout)
             if len(out) == 0:
                 continue
             q = ast.literal_eval(out)
             sender(q)
         except Exception as e:
-            print "Got exception running", x, ": ", e, ": ", out
+            logging.error('%s: exception: out: \'%s\', timeout: %d, error: %s',
+                    x, out, timeout, e)
             pass
-    time.sleep(10)
+
+    time.sleep(timeout)
