2d8f65d69892a0a40a9e1137d518db2b471e20c1
Evgeniy Polyakov <zbr@ioremap.net>
2015-01-05 21:31:21 +0300

    Added new test which periodically writes data and restart containers if needed.

---
+++ crocodile/conf.d/5-test_write.py
@@ -0,0 +1,217 @@
+#!/usr/bin/python
+
+import requests, json, time, sys, socket, bernhard
+import hmac
+import hashlib
+import urlparse
+import urllib
+import shutil
+import docker
+import logging
+
+logging.basicConfig(format='%(asctime)s %(levelname)s: test_writer: %(message)s',
+        level=logging.DEBUG)
+
+
+acl_file = '/etc/crocodile/acl.json'
+#acl_file = '/tmp/acl.json'
+acl_user = ''
+acl_token = ''
+
+def load_acl():
+    try:
+        with open(acl_file, 'r') as f:
+            j = json.load(f)
+            global acl_user, acl_token
+            acl_user = str(j['user'])
+            acl_token = str(j['token'])
+    except Exception as e:
+        logging.error("load_acl: file: %s, exception: %s", acl_file, e)
+        pass
+
+def generate_signature(key, method, url, headers=None):
+    parsed_url = urlparse.urlparse(url)
+    queries = urlparse.parse_qsl(parsed_url.query)
+    queries.sort()
+    text = ''
+    text += method + '\n'
+    text += parsed_url.path
+    if len(queries) > 0:
+        text += '?' + urllib.urlencode(queries)
+    text += '\n'
+    if headers:
+        headers = map(lambda x: (x[0].lower(), x[1]), headers.iteritems())
+        headers = filter(lambda x: x[0].startswith('x-ell-'), headers)
+        headers.sort()
+
+        for header in headers:
+            text += header[0] + ':' + header[1] + '\n'
+    return hmac.new(key, text, hashlib.sha512).hexdigest()
+
+def start_container(c):
+    start_params = {
+        'Image': 'reverbrain/backrunner:1.2.5',
+        'Ports': [80],
+        'Command': '/root/go/bin/backrunner -config /mnt/elliptics/etc/backrunner.conf -buckets /mnt/elliptics/etc/buckets.production',
+    }
+
+    new_cnt = c.create_container(
+            image           = start_params['Image'],
+            command         = start_params['Command'],
+            ports           = start_params['Ports'],
+            volumes         = ['/mnt/elliptics'],
+            )
+
+    nc = c.start(new_cnt['Id'],
+            binds = {
+                '/home/admin/elliptics' : {
+                    'bind':     '/mnt/elliptics',
+                    'ro':       False,
+                },
+            },
+            port_bindings = {
+                80: 80,
+            },
+            restart_policy = {
+                'Name': 'on-failure'
+            },
+    )
+
+    logging.info("new container has been started: %s", new_cnt)
+
+    message = {}
+    message['service'] = 'test_writer'
+    message['host'] = socket.getfqdn()
+    message['metric'] = 666
+    message['state'] = 'error'
+    message['description'] = 'new container has been started: %s' % (new_cnt)
+
+    return message
+
+def restart_proxy(clients):
+    try:
+        backrunner_log = '/home/admin/elliptics/log/backrunner.log'
+        new_log = backrunner_log + '.%d' % (time.time())
+        try:
+            shutil.move(backrunner_log, new_log)
+
+            logging.info("restart: log file has been moved: %s -> %s",
+                    backrunner_log, new_log)
+        except:
+            pass
+
+
+        need_new_container = True
+        message = {}
+        message['service'] = 'test_writer'
+        message['host'] = socket.getfqdn()
+        message['metric'] = 666
+        message['state'] = 'error'
+
+        c = docker.Client(base_url='unix://var/run/docker.sock')
+        try:
+            for cnt in c.containers():
+                if 'backrunner' in cnt['Command']:
+                    c.restart(cnt)
+                    message, need_restart = check_upload()
+
+                    if need_restart:
+                        c.stop(cnt['Id'])
+                        log.info("restart: container has been stopped: %s", cnt['Id'])
+                    else:
+                        log.info("restart: container has been restarted and works fine: %s", cnt['Id'])
+                        message['description'] = 'container has been restarted and wors fine: ' % (cnt['Id'])
+                        need_new_container = False
+        except Exception as e:
+            logging.error("restart: could not restart docker container: %s", e)
+
+        if need_new_container:
+            message = start_container(c)
+    except Exception as e:
+        logging.error("restart: could not start new docker container: %s", e)
+        message['description'] = 'could not start new docker container: %s' % (e)
+
+    return message
+
+def check_upload():
+    load_acl()
+    data = "test upload at: %s" % (time.time())
+
+    host = socket.getfqdn()
+    url = "http://" + host + "/nobucket_upload/test_writer.txt"
+
+    headers = {}
+    if acl_user != '' and acl_token != '':
+        headers['Authorization'] = 'riftv1 {0}:{1}'.format(
+                acl_user,
+                generate_signature(acl_token, 'POST', url))
+
+    message = {}
+    message['service'] = 'test_writer'
+    message['host'] = host
+
+    need_restart = False
+
+    try:
+        r = requests.post(url, data=data, headers=headers, timeout=5)
+        message['metric'] = r.status_code
+        message['state'] = 'info'
+
+        if r.status_code != requests.codes.ok:
+            message['state'] = 'error'
+            message['description'] = "%s" % r.text
+
+        if r.status_code == requests.codes.ok:
+            rep = r.json()
+            try:
+                sgroups = rep['reply']['success-groups']
+                egroups = rep['reply']['error-groups']
+                if len(sgroups) != 2:
+                    message['state'] = 'error'
+                    message['description'] = "not enough success results: %d instead of 2: %s" % (len(sgroups), r.text)
+                if len(egroups) != 0:
+                    message['state'] = 'error'
+                    message['description'] = "there are %d error results: %s" % (len(egroups), r.text)
+            except Exception as e:
+                    message['state'] = 'error'
+                    message['description'] = "could not parse reply: '%s': %s" % (r.text, e)
+
+    except Exception as e:
+        message['state'] = 'error'
+        message['description'] = "%s" % e
+        need_restart = True
+
+    logging.debug("check: need_restart: %s, message: %s",
+            need_restart, message)
+
+    return message, need_restart
+
+def upload_and_restart(clients):
+    message, need_restart = check_upload()
+    if need_restart:
+        message = restart_proxy(clients)
+
+    logging.info("upload_and_restart: message: %s", message)
+    send_all(clients, message)
+
+def send_all(clients, message):
+    for c in clients:
+        c.send(message)
+
+if __name__ == '__main__':
+    rlist = {}
+    for h in sys.argv[1].split(","):
+        k, v = h.split(":")
+        rlist[k] = v
+
+    clients = []
+    for rhost in rlist:
+        try:
+            c = bernhard.Client(host=rhost, port=rlist[rhost])
+            clients.append(c)
+        except Exception as e:
+            print "disk: host: %s, port: %s, exeption: %s" % (rhost,
+                    rlist[rhost], e)
+            pass
+
+    upload_and_restart(clients)
