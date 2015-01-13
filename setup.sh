dcd531611a206e080b9458a2701c577e59c43801
Evgeniy Polyakov <zbr@ioremap.net>
2015-01-13 17:48:39 +0300

    setup: get rid of useless commands

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 apt-get update >/dev/null
-apt-get install -y python-minimal python-pip git dos2unix
+apt-get install -y python-minimal python-pip git
 pip install protobuf bernhard supervisor
 wget https://raw.githubusercontent.com/bioothod/crocodile/master/crocodile/checker.sh
-dos2unix checker.sh
 bash checker.sh
 rm setup.sh checker.sh
