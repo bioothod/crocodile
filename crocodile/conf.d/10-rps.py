e7309612bde1d08cac0947651bf7698deb3438bc
Evgeniy Polyakov <zbr@ioremap.net>
2015-01-26 16:32:55 +0300

    rps: use stats from /proxy_stat/ handler, do not perform slow log parsing. ororo

--- crocodile/conf.d/10-rps.py
+++ crocodile/conf.d/10-rps.py
@@ -2,202 +2,159 @@
 
 import requests, json, time, sys, socket, bernhard
 import logging
-import os
-import re
 
 logging.basicConfig(filename='/var/log/supervisor/rps.log',
         format='%(asctime)s %(levelname)s: rps: %(message)s',
         level=logging.DEBUG)
 
-access_log_regexp = re.compile("(?P<prefix>.+): (?P<date>\\d+/\\d+/\\d+ \\d+:\\d+:\\d+)\\.(?P<usec>\\d+) access_log: method: '(?P<method>\\w+)', path: '(?P<path>.+)', encoded-uri: '(?P<uri>.+)', status: (?P<status>\\d+), size: (?P<size>\\d+), time: (?P<duration>[0-9\\.]+) ms, err: .*")
-generic_log_regexp = re.compile("(?P<prefix>.+): (?P<date>\\d+/\\d+/\\d+ \\d+:\\d+:\\d+)\\.(?P<usec>\\d+) .*")
+class parser:
+    def __init__(self, addresses):
+        self.acl_file = '/etc/crocodile/acl.json'
+        self.acl_user = ''
+        self.acl_token = ''
+        self.acl_base_dir = '/home/admin/elliptics'
+        self.acl_port = 80
+        self.acl_success_groups = 2
+        self.acl_port_mapping = {
+                80 : 80
+        }
+        self.clients = []
+        self.host = socket.getfqdn()
 
-acl_file = '/etc/crocodile/acl.json'
-#acl_file = '/tmp/acl.json'
-acl_base_dir = '/home/admin/elliptics'
+        self.load_acl()
+        return
 
-def load_acl():
-    try:
-        with open(acl_file, 'r') as f:
-            j = json.load(f)
-            global acl_base_dir
+        rlist = {}
+        for h in addresses.split(","):
+            k, v = h.split(":")
+            rlist[k] = v
 
-            base = j.get('base')
-            if base != None:
-                acl_base_dir = str(base)
-    except Exception as e:
-        logging.error("load_acl: file: %s, exception: %s", acl_file, e)
-        pass
-
-class entry:
-    def __init__(self):
-        self.date = time.time()
-        self.method = ''
-        self.path = ''
-        self.status = 0
-        self.size = 0
-        self.duration = 0.0
-
-    def __str__(self):
-        return "%s: path: %s, status: %d, size: %d" % (time.ctime(self.date), self.path, self.status, self.size)
-
-class metric:
-    def __init__(self, name):
-        self.name = name
-        self.requests = 0
-        self.duration = 0
-        self.size = 0
-        self.status = {}
-
-        self.status[200] = 0
-        self.status[300] = 0
-        self.status[400] = 0
-        self.status[500] = 0
-
-    def push(self, e):
-        self.size += e.size
-        self.requests += 1
-        self.duration += e.duration
-        if 200 <= e.status < 300:
-            self.status[200] += 1
-        elif 300 <= e.status < 400:
-            self.status[300] += 1
-        elif 400 <= e.status < 500:
-            self.status[400] += 1
-        elif 500 <= e.status < 600:
-            self.status[500] += 1
-
-    def message(self):
-        message = {}
-        message['host'] = socket.getfqdn()
-        message['state'] = 'info'
-
-        return message
-
-class estimator:
-    def __init__(self, clients, timing):
-        self.clients = clients
-        self.timing = timing
-        self.metrics = {}
-        prefix = ['/get/', '/upload/', '/nobucket_upload/', '/delete/']
-        for p in prefix:
-            self.metrics[p] = metric(p)
-
-    def push(self, e):
-        for k, m in self.metrics.iteritems():
-            if e.path.startswith(k):
-                self.metrics[k].push(e)
-                return
+        for rhost in rlist:
+            try:
+                c = bernhard.Client(host=rhost, port=rlist[rhost])
+                self.clients.append(c)
+            except Exception as e:
+                logging.error("init: host: %s, port: %s, exeption: %s" % (rhost, rlist[rhost], e))
+                pass
 
     def send_all(self, message):
-        logging.info(message)
-
+        logging.debug("send_all: %s", message)
         for c in self.clients:
             c.send(message)
 
-    def send(self):
-        for k, m in self.metrics.iteritems():
-            message = m.message()
-
-            for status, reqs in m.status.iteritems():
-                message['service'] = 'rps %s %s' % (status, m.name)
-                message['metric'] = reqs / self.timing
-                self.send_all(message)
-
-            message['service'] = 'bps %s' % (m.name)
-            message['metric'] = m.size / self.timing
-            self.send_all(message)
-
-def parse_chunk(chunk, boundary_ts):
-    have_events = False
-    for m in access_log_regexp.finditer(chunk):
-        tm = time.mktime(time.strptime(m.group('date'), '%Y/%m/%d %H:%M:%S'))
-
-        e = entry()
-        e.date = tm
-        e.method = m.group('method')
-        e.path = m.group('path')
-        e.status = int(m.group('status'))
-        e.size = int(m.group('size'))
-        e.duration = float(m.group('duration'))
-
-        have_events = True
-
-        yield e
-
-        if tm < boundary_ts:
-            return
-
-    if not have_events:
-        for m in generic_log_regexp.finditer(chunk):
-            tm = time.mktime(time.strptime(m.group('date'), '%Y/%m/%d %H:%M:%S'))
-
-
-            # send a special empty entry to parser loop to specify that it is time
-            # to stop waiting for more events
-            if tm < boundary_ts:
-                e = entry()
-                e.date = tm
-
-                yield e
-                return
-
-def read_and_parse(clients, timing):
-    load_acl()
-
-    logfile = '%s/log/backrunner.log' % (acl_base_dir)
-
-    boundary_timestamp = time.time() - timing
-
-    bsize = 1024 * 16
-
-    est = estimator(clients, timing)
-
-    with open(logfile, 'r') as f:
-        f.seek(0, os.SEEK_END)
-
-        need_exit = False
-
-        while not need_exit:
-            f.seek(-bsize, os.SEEK_CUR)
-            offset = f.tell()
-            chunk = f.read(bsize)
-            f.seek(-bsize, os.SEEK_CUR)
-
-            # we do not really care if chunk boundary splits access_log line into parts
-
-            for e in parse_chunk(chunk, boundary_timestamp):
-                if e.date < boundary_timestamp:
-                    need_exit = True
-                    break
-
-                est.push(e)
-
-            # we are at the beginning of the file
-            if len(chunk) != bsize:
-                need_exit = True
-                break
-
-    est.send()
-
-if __name__ == '__main__':
-    tm = 5
-    m = re.search('^\d+', os.path.basename(sys.argv[0]))
-    if m != None:
-        tm = int(m.group(0))
-
-    rlist = {}
-    for h in sys.argv[1].split(","):
-        k, v = h.split(":")
-        rlist[k] = v
-
-    clients = []
-    for rhost in rlist:
+    def load_acl(self):
         try:
-            c = bernhard.Client(host=rhost, port=rlist[rhost])
-            clients.append(c)
+            with open(self.acl_file, 'r') as f:
+                j = json.load(f)
+                self.acl_user = str(j['user'])
+                self.acl_token = str(j['token'])
+                port = j.get('port')
+                if port != None:
+                    self.acl_port = int(port)
+
+                success_groups = j.get('success_groups')
+                if success_groups != None:
+                    self.acl_success_groups = int(success_groups)
+
+                pm = j.get('port_mapping')
+                if pm != None:
+                    self.acl_port_mapping = {}
+                    for k, v in pm.items():
+                        self.acl_port_mapping[int(k)] = int(v)
+
+                base = j.get('base')
+                if base != None:
+                    self.acl_base_dir = str(base)
         except Exception as e:
-            logging.error("rps: host: %s, port: %s, exeption: %s" % (rhost, rlist[rhost], e))
+            logging.error("load_acl: file: %s, exception: %s", self.acl_file, e)
             pass
 
-    read_and_parse(clients, tm)
+    def generate_signature(self, key, method, url, headers=None):
+        parsed_url = urlparse.urlparse(url)
+        queries = urlparse.parse_qsl(parsed_url.query)
+        queries.sort()
+        text = ''
+        text += method + '\n'
+        text += parsed_url.path
+        if len(queries) > 0:
+            text += '?' + urllib.urlencode(queries)
+        text += '\n'
+        if headers:
+            headers = map(lambda x: (x[0].lower(), x[1]), headers.iteritems())
+            headers = filter(lambda x: x[0].startswith('x-ell-'), headers)
+            headers.sort()
+
+            for header in headers:
+                text += header[0] + ':' + header[1] + '\n'
+        return hmac.new(key, text, hashlib.sha512).hexdigest()
+
+    def send_error_message(self, metric, description):
+        message = {}
+        message['service'] = 'rps'
+        message['host'] = self.host
+        message['state'] = 'error'
+        message['metric'] = metric
+        message['description'] = description
+
+        logging.error("send_error_message: %s", message['description'])
+        self.send_all(message)
+
+    def proxy_stat(self):
+        url = "http://%s:%d/proxy_stat/" % (self.host, self.acl_port)
+
+        headers = {}
+        if self.acl_user != '' and self.acl_token != '':
+            headers['Authorization'] = 'riftv1 {0}:{1}'.format(
+                    self.acl_user,
+                    generate_signature(self.acl_token, 'GET', url))
+
+        text = ''
+        try:
+            r = requests.get(url, headers=headers, timeout=5)
+            if r.status_code != requests.codes.ok:
+                self.send_error_message(r.status_code, "invalid status code: %d: %s" % (r.status_code, r.text))
+                return
+            text = r.text
+        except Exception as e:
+            self.send_error_message(666, "could not read proxy stat: %s" % e)
+            return
+
+        self.parse_proxy_stats(text)
+
+    def parse_proxy_stats(self, text):
+        j = json.loads(text)
+
+        handlers = j.get('handlers')
+        if handlers == None:
+            self.send_error_message(200, "invalid json in reply: no 'handlers': '%s'" % text)
+            return
+
+        message = {}
+        message['host'] = self.host
+        message['state'] = 'info'
+
+        for hname, counters in handlers.items():
+            bps = counters.get('bps')
+            if bps == None:
+                self.send_error_message(200, "invalid json in reply: no 'bps': '%s'" % text)
+                return
+
+            message['service'] = 'bps %s' % (hname)
+            message['metric'] = bps
+            self.send_all(message)
+
+            rps = counters.get('rps')
+            if rps == None:
+                self.send_error_message(200, "invalid json in reply: no 'rps': '%s'" % text)
+                return
+
+            if len(rps) != 0:
+                for status, cnt in rps.items():
+                    message['service'] = 'rps %s %s' % (status, hname)
+                    message['metric'] = cnt
+                    self.send_all(message)
+
+
+if __name__ == '__main__':
+    p = parser(sys.argv[1])
+    p.proxy_stat()
