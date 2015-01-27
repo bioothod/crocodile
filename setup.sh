fd03d8e7803837bcbfd7b705c1fc5853a6e29dae
Evgeniy Polyakov <zbr@ioremap.net>
2015-01-27 03:47:53 +0300

    setup: added docker isntallation via its own scripts

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-apt-get update >/dev/null
+#curl -sSL https://get.docker.com/ubuntu/ | sh
+curl -sSL https://get.docker.com/ | sh
+
+apt-get update
 apt-get install -y python-minimal git lxc-docker python-dev
 easy_install pip
 pip install protobuf bernhard supervisor docker-py psutil
