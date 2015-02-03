170c444f1421bcbfc101c8cfa20f35920b4a5ba8
Evgeniy Polyakov <zbr@ioremap.net>
2015-02-03 16:34:11 +0300

    dmesg: regexp cleanup

--- crocodile/conf.d/600-dmesg.py
+++ crocodile/conf.d/600-dmesg.py
@@ -9,9 +9,9 @@ import re
 import os, io, fcntl
 import time
 
-re_WARN=re.compile('error|warning|fail|\(da[0-9]+:[a-z0-9]+:[0-9]+:[0-9]+:[0-9]+\)|Non-Fatal\ Error\ DRAM\ Controler|nf_conntrack')
-re_IGNORE=re.compile('acpi|ehci_hcd|uses\ 32-bit\ capabilities|GHES:\ Poll\ interval|acpi_throttle[0-9]:\ failed\ to\ attach\ P_CNT|aer|aer_init:\ AER\ service\ init\ fails|aer:\ probe\ of|arplookup|(at [0-9a-f]+)? rip[: ][0-9a-f]+ rsp[: ][0-9a-f]+ error[: ]|Attempt\ to\ query\ device\ size\ failed|check_tsc_sync_source\ failed|failed\ SYNCOOKIE\ authentication|igb:|ipfw:\ pullup\ failed|Marking\ TSC\|mfi|MOD_LOAD|MSI\ interrupts|nfs\ send\ error\ 32\ |NO_REBOOT|optimal|page\ allocation\ failure|PCI\ error\ interrupt|pid\ [0-9]+|rebuild|Resume\ from\ disk\ failed|rwsem_down|smb|swap_pager_getswapspace|thr_sleep|uhub[0-9]|ukbd|usbd|EDID|radeon|drm:r100_cp_init|Opts: errors=remount-ro|nf_conntrack version')
-re_CRIT=re.compile('I/O error|medium|defect|mechanical|retrying|broken|degraded|offline|failed|unconfigured_bad|conntrack:\ table\ full|Unrecovered read error|fs error|Medium Error|Drive ECC error|Out of memory|Killed process|oom-killer')
+re_WARN=re.compile('error|warning|fail|\(da[0-9]+:[a-z0-9]+:[0-9]+:[0-9]+:[0-9]+\)|Non-Fatal Error DRAM Controler|nf_conntrack')
+re_IGNORE=re.compile('acpi|ehci_hcd|uses 32-bit capabilities|GHES: Poll interval|acpi_throttle[0-9]: failed to attach P_CNT|aer|aer_init: AER service init fails|aer: probe of|arplookup|(at [0-9a-f]+)? rip[: ][0-9a-f]+ rsp[: ][0-9a-f]+ error[: ]|Attempt to query device size failed|check_tsc_sync_source failed|failed SYNCOOKIE authentication|igb:|ipfw: pullup failed|Marking TSC|mfi|MOD_LOAD|MSI interrupts|nfs send error 32 |NO_REBOOT|optimal|page allocation failure|PCI error interrupt|pid [0-9]+|rebuild|Resume from disk failed|rwsem_down|smb|swap_pager_getswapspace|thr_sleep|uhub[0-9]|ukbd|usbd|EDID|radeon|drm:r100_cp_init|Opts: errors=remount-ro|nf_conntrack version')
+re_CRIT=re.compile('I/O error|medium|defect|mechanical|retrying|broken|degraded|offline|failed|unconfigured_bad|conntrack: table full|Unrecovered read error|fs error|Medium Error|Drive ECC error|Out of memory|Killed process|oom-killer')
 re_dmesg_line=re.compile('^(\d+),(\d+),(?P<timestamp>\d+),(.*);')
 
 class dmesg_parser(noscript_parser.parser):
