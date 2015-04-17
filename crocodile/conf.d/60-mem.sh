688b28741698c40327b6d8f21b964ccbcab42f71
Evgeniy Polyakov <zbr@ioremap.net>
2015-04-17 18:42:34 +0300

    mem: put dnet_ioserv gdb backtrace when memory error occurs. ororo

--- crocodile/conf.d/60-mem.sh
+++ crocodile/conf.d/60-mem.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 #check free mem
+
 error_perc=20
 warning_perc=30
 total=`free -b |grep Mem |awk {' print $2 '}`
 free=`free -b |grep buffers/cache |awk {' print $4 '}`
-if [ $(($free * 100 / $total)) -le $error_perc ];then 
-  state='error'
-elif [ $(($free * 100 / $total)) -le $warning_perc ];then 
-  state='warning'
+trace=""
+
+if [ $(($free * 100 / $total)) -le $error_perc ]; then
+	trace=`gdb -ex "set pagination 0" -ex "thread apply all bt" --batch -p $(pidof dnet_ioserv)`
+	state='error'
+elif [ $(($free * 100 / $total)) -le $warning_perc ]; then
+	state='warning'
 else
-  state='normal'
+	state='normal'
 fi
-echo "{'host':'`hostname -f`', 'service':'memory','metric':$free,'state':'$state'}"
+
+echo "{'host':'`hostname -f`', 'service':'memory', 'metric':$free, 'state':'$state', 'description':'$trace'}"
