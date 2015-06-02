c43d352408f74aa3089074a0e489698164e1f57c
Evgeniy Polyakov <zbr@ioremap.net>
2015-06-02 15:17:22 +0300

    setup: added 'disitrbute' install, it is needed for proper supervisor installation

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ curl -sSL https://get.docker.com/ | sh
 apt-get update
 apt-get install -y python-minimal git lxc-docker python-dev python-setuptools elliptics gdb
 easy_install pip
+pip install distribute --upgrade
 pip install protobuf bernhard distribute supervisor docker-py psutil
 wget https://raw.githubusercontent.com/bioothod/crocodile/master/crocodile/checker.sh
 bash checker.sh
