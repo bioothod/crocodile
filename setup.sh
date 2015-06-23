cd81ab7f83c3c72807f769d902ed4f198b1f3a9a
Evgeniy Polyakov <zbr@ioremap.net>
2015-06-23 16:54:01 +0300

    setup: added ntp into default installation

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 curl -sSL https://get.docker.com/ | sh
 
 apt-get update
-apt-get install -y python-minimal git lxc-docker python-dev python-setuptools elliptics gdb
+apt-get install -y python-minimal git lxc-docker python-dev python-setuptools elliptics gdb ntp
 easy_install pip
 pip install distribute --upgrade
 pip install protobuf bernhard distribute supervisor docker-py psutil
