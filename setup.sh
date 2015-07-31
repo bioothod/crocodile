aa9a5158b5de3b20396641b2fd3dceaa2e71a1ac
Evgeniy Polyakov <zbr@ioremap.net>
2015-08-01 01:21:31 +0300

    setup: docker was renamed to docker-engine

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@
 curl -sSL https://get.docker.com/ | sh
 
 apt-get update
-apt-get install -y python-minimal git lxc-docker python-dev python-setuptools elliptics gdb ntp
+apt-get install docker-engine
+apt-get install -y python-minimal git python-dev python-setuptools elliptics gdb ntp
 easy_install pip
 pip install distribute --upgrade
 pip install protobuf bernhard distribute supervisor docker-py psutil
