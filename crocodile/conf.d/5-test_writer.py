2b74623558a07f1eb4633954ab6dac4ed3a52d73
Evgeniy Polyakov <zbr@ioremap.net>
2015-08-21 16:17:18 +0300

    test_writer: added GODEBUG option to disable stack barrier, otherwise backrunner/go crashes. ororo

--- crocodile/conf.d/5-test_writer.py
+++ crocodile/conf.d/5-test_writer.py
@@ -31,7 +31,7 @@ class test_writer(noscript_parser.parser):
                 environment     = {
                     'GOGC1': '100',
                     'GOTRACEBACK': 'crash',
-                    'GODEBUG': 'invalidptr=0'
+                    'GODEBUG': 'invalidptr=0,gcstackbarrieroff=1'
                 }
         )
 
