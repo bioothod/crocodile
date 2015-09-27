ce4be8ebb1b3913e9cd4c9bf2658798b2d1606c6
Evgeniy Polyakov <zbr@ioremap.net>
2015-09-27 22:06:25 +0300

    setup: added psmisc which contains killall util

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ curl -sSL https://get.docker.com/ | sh
 
 apt-get update
 apt-get install docker-engine
-apt-get install -y python-minimal git python-dev python-setuptools elliptics gdb ntp dstat ethtool
+apt-get install -y python-minimal git python-dev python-setuptools elliptics gdb ntp dstat ethtool psmisc
 easy_install pip
 pip install distribute --upgrade
 pip install protobuf bernhard distribute supervisor docker-py psutil
